6ee8d72205b8ac30ee296fa97eac59e82ec4bec2
Dan <d.epstein92@gmail.com>
2017-09-26 12:23:24 -0400

    Home Page done, working on Blog Posts

---
+++ 5-6.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "posts" DROP COLUMN description;
+ALTER TABLE "posts" ADD COLUMN description VARCHAR(500);
---
+++ effectsMain.js
@@ -0,0 +1,7 @@
+var designs_logo = document.getElementById('font-effect-neon');
+
+anime({
+  targets: designs_logo,
+  backgroundColor: '#FFF',
+  duration: 2000
+})
